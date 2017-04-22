72602d57b1845d08140535a384cfc7269933692f
Adam Douglass <douglass@malloc.ca>
2017-04-22 11:45:39 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Modest-Mudcake/Library/InspectorExpandedItems.asset
    #	Modest-Mudcake/Library/assetDatabase3
    
    Add a menu?

---
+++ Modest-Mudcake/Assets/dragableTile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragableTile : MonoBehaviour 
+{
+    public bool dragable = true;
+    public bool held = false;
+	
+	void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    void OnMouseDown()
+    {
+        held = true;
+        if (dragable)
+        {
+            
+        }
+    }
+
+    void OnMouseUp()
+    {
+        held = false;
+        
+        if (dragable)
+        {
+            // attempt to socket to nearest game slot
+        }
+    }
+}
