f5f22e0234116637ba561a328e0c828bc53df9e5
matt <matt@matthewmason.ca>
2017-04-22 15:54:46 -0300

    the drag and drop commit

--- Modest-Mudcake/Assets/dragableTile.cs
+++ Modest-Mudcake/Assets/dragableTile.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class dragableTile : MonoBehaviour 
 {
     public bool dragable = true;
+    public float snapDistance = 1.0f;
+
+    public GameObject board = null;
+
+    private Vector3 originalPosition;
     public bool held = false;
-	
+
 	void Start () 
     {
 		
@@ -19,10 +24,10 @@ public class dragableTile : MonoBehaviour
 
     void OnMouseDown()
     {
-        held = true;
         if (dragable)
         {
-            
+            held = true;
+            originalPosition = this.transform.position;
         }
     }
 
@@ -32,7 +37,54 @@ public class dragableTile : MonoBehaviour
         
         if (dragable)
         {
-            // attempt to socket to nearest game slot
+            Transform nearest = nearestSocket();
+
+            if (nearest != null)
+            {
+                if (Vector3.Distance(this.transform.position, nearest.position) < snapDistance)
+                {
+                    this.transform.position = nearest.position;
+                }
+                else
+                {
+                    this.transform.position = originalPosition;
+                }
+            }
         }
+    }
+
+    void OnMouseDrag()
+    {
+        if (dragable)
+        {
+            float x = Input.mousePosition.x;
+            float y = Input.mousePosition.y;
+
+            this.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x, y, Camera.main.transform.position.z * -1));
+        }
+    }
+
+    private Transform nearestSocket()
+    {
+        Transform nearest = null;
+
+        foreach (Transform t in board.GetComponentInChildren<Transform>())
+        {
+            if (t == board.transform)
+                continue;
+
+            if (nearest == null)
+            {
+                nearest = t;
+            }
+
+            if (Vector3.Distance(this.transform.position, t.position) 
+                < Vector3.Distance(this.transform.position, nearest.position))
+            {
+                nearest = t;
+            }
+        }
+
+        return nearest;
     }
 }
