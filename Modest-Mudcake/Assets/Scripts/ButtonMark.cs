fa1950534073cffa63c1724e062ea0ae4b095e02
Adam Douglass <douglass@malloc.ca>
2017-04-23 16:53:21 -0700

    Only unlock levels as some before are done.

--- Modest-Mudcake/Assets/Scripts/ButtonMark.cs
+++ Modest-Mudcake/Assets/Scripts/ButtonMark.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class ButtonMark : MonoBehaviour {
 
 	public int levelNumber = -1;
+//	public int unlockLevel = 0;
+	public static int totalLevels = 10;
+
 	public GameObject doneMark = null;
 	protected const string keyFormat = "level-finished-{0}";
 
 	// Use this for initialization
 	void Start () {
+//		PlayerPrefs.DeleteAll ();
+		int unlockLevel = Mathf.Max(0, levelNumber - 3);
+
 		if (levelNumber < 0)
 			Debug.LogError ("A Level number hasn't been set for one of the button mark scripts");
 
-		if (PlayerPrefs.GetInt (string.Format (keyFormat, levelNumber)) > 0) {
+		if (IsFinished(levelNumber)) {
 			Instantiate (doneMark, gameObject.transform);
 		} 
+
+		if (unlockLevel > CountFinished ()) {
+			gameObject.GetComponent<UnityEngine.UI.Button>().interactable = false;
+		}
+	}
+
+	public static int CountFinished(){
+		int count = 0;
+		for (int ii = 1; ii <= totalLevels; ii++) {
+			count += IsFinished(ii) ? 1 : 0;
+		}
+		return count;
+	}
+
+	public static bool IsFinished(int number){
+		return PlayerPrefs.GetInt (string.Format (keyFormat, number)) > 0;
 	}
 
 	public static void FinishedLevel(int number){
